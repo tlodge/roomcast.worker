e66328c4a672abb4dba77d908a205511c5ab257a
tlodge <tlodge@gmail.com>
2015-11-25 15:52:02 +0000

    first commit

---
+++ worker.js
@@ -0,0 +1,40 @@
+var amqp= require('amqplib');
+var open = require('amqplib').connect('amqp://localhost')
+var request = require('superagent');
+
+open.then(function(conn){
+  console.log("OPENED");
+  process.once('SIGINT', function(){conn.close();});
+
+  return conn.createChannel().then(function(ch){
+      var ok = ch.assertQueue('tasks');
+
+      ok = ok.then(function(_qok){
+
+          return ch.consume('tasks', function(msg){
+                console.log("[x] received object '%s'", msg.content);
+
+
+                var endpoint = JSON.parse(msg.content);//{mymessage:'hello'};
+                console.log(endpoint);
+                //'/red/posttest'
+                request.post(endpoint.url)
+  		                 .send(endpoint.parameters)
+  		                 .set('Accept', 'application/json')
+  		                 .type('json')
+  		                 .end(function(err, res){
+                  			if (err){
+                  				console.log(err);
+                  			}else{
+                  				return "done";
+                  	 		}
+  	 	          });
+          }, {noAck: true});
+      });
+
+      return ok.then(function(_consumeOk){
+            console.log(" [*] waiting for messages.  To exit press ctrl-c");
+
+      });
+  });
+}).then(null, console.warn);
