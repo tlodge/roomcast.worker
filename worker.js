ae77b1071c150080538fce12e316913af1cb9ddf
tlodge <tlodge@gmail.com>
2018-10-30 19:33:08 +0000

    preserve ordering

--- worker.js
+++ worker.js
@@ -1,4 +1,3 @@
-var amqp= require('amqplib');
 var ampqlib = require('amqplib');
 var request = require('superagent');
 var path = require('path');
@@ -7,72 +6,102 @@ var commandLineArgs = require('command-line-args');
 
 
 var optionDefinitions = [
-  { name: 'config',  alias: "f", type: String, multiple: false, defaultOption: true, defaultValue:path.join(__dirname, './conf/config.json')},
+  { name: 'config', alias: "f", type: String, multiple: false, defaultOption: true, defaultValue: path.join(__dirname, './conf/config.json') },
 ]
 //var configFile = process.argv.length > 2 ? 
 var options = commandLineArgs(optionDefinitions);
 var configFile = options.config;
 var config = JSON.parse(fs.readFileSync(configFile).toString());
-console.log("running with config",config);
+console.log("running with config", config);
 
-var defaultHeaders = {Accept: 'application/json'};
+var defaultHeaders = { Accept: 'application/json' };
 
-var connect = function(){
-  try{
-      ampqlib.connect(config.amqpurl).then(function(conn){
+var reconnect = function () {
+  console.log("reconnecting in 5s");
+  setTimeout(connect, 5000);
+}
+
+var connect = function () {
+  try {
+    ampqlib.connect(config.amqpurl).then(function (conn) {
       listen(conn);
-   },function(err){
-      console.log("hmm--- errror!!!");
-      setTimeout(connect, 5000);
-   });
-  }catch(err){
-      console.log("error connecting - will try again!");
-      setTimeout(connect, 5000);
+    }, function (err) {
+      reconnect();
+    });
+  } catch (err) {
+    reconnect();
   }
 }
 
-var listen = function(conn){
-  console.log("starting to listen");
-  process.once('SIGINT', function(){
-        console.log("seen a SIGINT,closing connection and trying reconnect");
-	conn.close();
-        setTimeout(connect, 5000);
+var listen = function (conn) {
+
+  console.log("worker successfully listening");
+
+  process.once('SIGINT', function () {
+    console.log("seen a SIGINT,closing connection and trying reconnect");
+    conn.close();
+    reconnect();
   });
 
-  return conn.createChannel().then(function(ch){
-      var ok = ch.assertQueue('tasks');
+  return conn.createChannel().then(function (ch) {
 
-      ok = ok.then(function(_qok){
+    const post = (msg) => {
+      console.log("[x] received object '%s'", msg.content);
+      var endpoint = JSON.parse(msg.content);//{mymessage:'hello'};
+      var headers = msg.headers || defaultHeaders;
 
-          return ch.consume('tasks', function(msg){
-                console.log("[x] received object '%s'", msg.content);
-                console.log("message headers are");
-                console.log(msg.headers);
+      request.post(endpoint.url)
+        .send(endpoint.parameters)
+        .set(headers)
+        .type(endpoint.format || 'json')
+        .end(function (err, res) {
+          if (err) {
+            console.log(err);
+            ch.ack(msg);
+          } else {
+            console.log("posted sucessfully!", endpoint.url);
+            console.log(res.body);
+            ch.ack(msg);
+          }
+        });
+    }
 
-                var endpoint = JSON.parse(msg.content);//{mymessage:'hello'};
-              
-                var headers = msg.headers || defaultHeaders;
-                request.post(endpoint.url)
-                       .send(endpoint.parameters)
-                       .set(headers)
-                       .type(endpoint.format || 'json')
-                       .end(function(err, res){
-                        if (err){
-                          console.log(err);
-                        }else{
-                          return "done";
-                        }
-                });
-          }, {noAck: true});
-      });
 
-      return ok.then(function(_consumeOk){
-            console.log(" [*] waiting for messages.  To exit press ctrl-c");
-
-      },function(err){
-         console.log("error, reconnecting in 5");
-	 setTimeout(connect, 5000);		
-      });
+    ch.assertQueue('tasks').then(function () {
+      ch.prefetch(1);
+      return ch.consume('tasks', post);
+    }).then(function () {
+      console.log(' [*] Awaiting RPC requests');
+    }, (err) => {
+      console.log(err);
+      reconnect();
+    }).catch((err) => {
+      console.log(err);
+      reconnect();
+    });
   });
+
 }
+
 connect();
+
+    /*var ok = ch.assertQueue('tasks');
+
+    ok.then(function (_qok) {
+
+      return ch.consume('tasks', function (msg) {
+        console.log("[x] received object '%s'", msg.content);
+        ch.prefetch(1);
+        return ch.consume('tasks', msg);
+      }, { noAck: true });
+    }).then(function (_consumeOk) {
+      console.log(" [*] waiting for messages.  To exit press ctrl-c");
+    }, function (err) {
+      console.log(err);
+      reconnect();
+    })
+  }).catch((err) => {
+    console.log(err);
+    reconnect();
+  });
+}*/
